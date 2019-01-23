b5067278dc8daa85a4a8c143cd9983a2d0da8ab3
12bod <12bod@DESKTOP-3Q44HCE>
2019-01-23 06:58:42 +0200

    parse data page

--- Advant/AdvantParse.cs
+++ Advant/AdvantParse.cs
@@ -9,7 +9,7 @@ namespace Advant
 {
     public class AdvantParse
     {
-       // private Regex regNum = new Regex(@"[^\d]");
+        private Regex regNum = new Regex(@"[^\d]");
         private string Cookie { get; set; }
         private string Proxy { get; set; }
         private AdvantWeb _web;
@@ -39,6 +39,7 @@ namespace Advant
             foreach (var url in urlsTour)
             {
                 var htmlTour = await _web.GetPageTour(url, Cookie, Proxy);
+                var data = FillingData(htmlTour);
             }
 
 
@@ -67,6 +68,61 @@ namespace Advant
             var list = collectDiv.Select(x => x?.SelectSingleNode("//a").Attributes["href"].Value ?? "").ToList();
 
             return list;
+        }
+
+        private DataOutput FillingData(HtmlDocument html)
+        {
+            DataOutput data = new DataOutput();
+
+            var names = html.DocumentNode.SelectSingleNode("//p[@class='fz18 mb10']").InnerText.Replace("\n", "").Replace("\t", "");
+            var arrName = names.Split(',');
+            data.CountryTo = arrName[1];
+            data.CityTo = arrName[0];
+
+            var collTagMB20 = html.DocumentNode.SelectNodes("//li[@class='mb20']");
+            foreach (var tag in collTagMB20)
+            {
+                var type = tag.SelectSingleNode(".//span").InnerText;
+                var value = tag.SelectSingleNode(".//p").InnerText;
+
+                switch (type)
+                {
+                    case "Туристы":
+                        data.CountAdults = regNum.Match(value).Value;
+                        break;
+                    case "Питание":
+                        data.FoodBig = value;
+                        break;
+                    case "Номер":
+                        data.TypeRoomBig = value;
+                        break;
+                    case "Ночей в туре":
+                        data.CountNight = regNum.Match(value).Value;
+                        break;
+                }
+            }
+
+            var collTagMBDown = html.DocumentNode.SelectNodes("//li[@class='mb-down-md-20']");
+            foreach (var tag in collTagMBDown)
+            {
+                var type = tag.SelectSingleNode(".//span").InnerText;
+                var value = tag.SelectSingleNode(".//p").InnerText;
+
+                if (type.Equals("Город вылета"))
+                {
+                    data.CityFrom = value;
+                }
+                else if (type.Equals("Дата вылета"))
+                {
+                    //data.DataFrom
+                }
+            }
+
+            data.Operator = html.DocumentNode.SelectSingleNode("//li[@class='d-block d-md-none']").InnerText;
+
+            data.Price = html.DocumentNode.SelectSingleNode("//span[@class='fz24']").InnerText;
+
+            return data;
         }
     }
 }
